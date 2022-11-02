3e529626e3881f22ef6bcc725decafe32cac668a
ariqluth <ttzluthfi@gmail.com>
2022-11-02 17:43:25 +0700

    composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6a531ac54736e2b8b3ecbacc274848b922d23461',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6a531ac54736e2b8b3ecbacc274848b922d23461',
+    ),
+  ),
+);
